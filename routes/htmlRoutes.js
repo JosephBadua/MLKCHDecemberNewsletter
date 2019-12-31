bcaf60c24089202d2bc73088b28cc900943954fb
JosephBadua <josephbadua1006@gmail.com>
2019-12-30 18:48:33 -0800

    added january base

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
+        res.render("january");
+    });
+    app.get("/december", function(req, res) {
         res.render("index");
     });
 };
