228c6516781ac6191b3cf6d220d5e2ad7f60d92c
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-01-02 15:30:27 -0800

    unlive

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     // Load index page
-    app.get("/", function(req, res) {
+    app.get("", function(req, res) {
         res.render("january");
     });
     app.get("/december", function(req, res) {
