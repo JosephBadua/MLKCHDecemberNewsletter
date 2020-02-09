287527c7384a17160e6feae55532f7dc9adf1edc
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-02-08 17:41:28 -0800

    added feb page, homepage, and calendar

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
+        res.render("february");
+    });
+    app.get("/january", function(req, res) {
         res.render("january");
     });
     app.get("/december", function(req, res) {
