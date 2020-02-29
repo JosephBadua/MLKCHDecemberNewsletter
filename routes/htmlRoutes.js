42cc5eee72d9dc30b9ae02efbe74a802d576b535
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-02-29 02:10:04 -0800

    finishing up march

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,10 @@
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
-        res.render("february");
-    });
-    app.get("/marchWIP", function(req, res) {
         res.render("march");
+    });
+    app.get("/february", function(req, res) {
+        res.render("february");
     });
     app.get("/january", function(req, res) {
         res.render("january");
