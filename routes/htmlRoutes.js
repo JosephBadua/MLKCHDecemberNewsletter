5d5696012ee6db8abb93929c129f951a105130ff
JosephBadua <josephbadua1006@gmail.com>
2020-05-06 17:58:16 -0700

    apirl newsletter 1.0

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
+        res.render("may");
+    });
+    app.get("/april", function(req, res) {
         res.render("april");
     });
     app.get("/march", function(req, res) {
